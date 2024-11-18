bc0a0abd59dfc46ffaf624af6346951eaa2a1622
udhaya106uk <122509195+udhaya106uk@users.noreply.github.com>
2024-11-18 13:02:34 +0530

    Main class

---
+++ HrServiceApplication.java
@@ -0,0 +1,29 @@
+package com.onward.hrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.onward.hrservice.repository")
+public class HrServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+						.allowedHeaders("*");
+			}
+		};
+	}
+
+}
